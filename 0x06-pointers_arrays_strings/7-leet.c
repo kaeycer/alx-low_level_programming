db378e5cd7d3a046be14393fc1831ce5360bafb9
Mpumelelo Mayisela <mayiselampumelelo130@gmail.com>
2024-01-05 17:48:11 -0800

    no7.1

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,26 +2,30 @@
 #include "main.h"
 
 /**
- * leet - encodes a string into 1337
+ * *leet - encodes a string into 1337
  * @n: no1
  *
  * Return: a pointer
  */
-char *leet(char *)
+char *leet(char *n)
 {
 	int i, j;
 	char s1[] = "aAeEoOtTlL";
 	char s2[] = "4433007711";
 
-	for (i = 0; n[i] != '\0'; i++)
+	i = 0;
+	while (n[i] != '\0')
 	{
-		for (j = 0; j < 10; j++)
+		j = 0;
+		while (j < 10)
 		{
 			if (n[i] == s1[j])
 			{
 				n[i] = s2[j];
 			}
+			j++;
 		}
+		i++;
 	}
 	return (n);
 }
