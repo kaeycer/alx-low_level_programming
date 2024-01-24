a34cce99fe2f269856c1376ffc3dfa8536b0ecd1
Mpumelelo Mayisela <mayiselampumelelo130@gmail.com>
2024-01-23 19:29:59 -0800

    Tried task 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - gives a dogs info
+ * @name: first par
+ * @age: second par
+ * @owner: third par
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
