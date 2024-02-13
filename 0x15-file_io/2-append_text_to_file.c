deb9bfd58618d72b72523a471215ce9688931d25
Mpumelelo Mayisela <mayiselampumelelo130@gmail.com>
2024-02-12 19:59:22 -0800

    f

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * append_text_to_file - his function creates a file
+ * @filename: the name of a file
+ * @text_content: the content of the file
+ *
+ * Return: an int val
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fp, wt, i = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[i] != '\0')
+			i++;
+	}
+
+	fp = open(filename, O_WRONLY | O_APPEND);
+	wt = write(fp, text_content, i);
+	
+	if (fp == -1 || wt == -1)
+		return (0);
+
+	close(fp);
+	return (-1);
+}
