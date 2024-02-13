478598349bd6f684cbe2db55dfe88ba99905a486
Mpumelelo Mayisela <73696205+kaeycer@users.noreply.github.com>
2024-02-13 04:50:37 +0200

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/start.h>
-#include <fcnti.h>
+#include <fcntl.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
