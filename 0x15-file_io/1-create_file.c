97d7a0f8ea5aba9af228ef06b9594d84c70c5aec
Mpumelelo Mayisela <73696205+kaeycer@users.noreply.github.com>
2024-02-17 05:45:04 +0200

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,8 +29,6 @@ int create_file(const char *filename, char *text_content)
 	if (fp == -1 || wt == -1)
 		return (-1);
 
-	int success = close(fp);
-	if (success == -1)
-		return (-1);
+	close(fp);
 	return (1);
 }
