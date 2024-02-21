24cb038a87a30f0ddb108ec59cafb790a41a1009
Mpumelelo <mayiselampumelelo130@gmail.com>
2024-02-21 02:13:15 -0800

    This is the path

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int i;
+	char n;
+
+	i = 48;
+	while (i <= 57)
+	{
+			putchar(i);
+			i++;
+	}
+	n = 'a';
+	while (n <= 'f')
+	{
+			putchar(n);
+			n++;
+	}
+	putchar('\n');
+	return (0);
+}
